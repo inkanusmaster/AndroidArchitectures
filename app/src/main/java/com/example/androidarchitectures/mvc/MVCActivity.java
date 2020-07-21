cc90d8fff805145381cd7917a920e42f5ea7f47e
inkanusmaster <wojcicki.m89@gmail.com>
2020-07-21 14:24:17 +0200

    Po wstępie teoretycznym do MVC. ZRÓB TO W ŚCIĄDZE!!! Z RYSUNKIEM!!! A oto konkluzja.
    MVC - MODEL VIEW CONTROLLER
    
    VIEW - to widok czyli to co daje nam tutaj MVCActivity. To się robi jako pierwsze przez nasz sytem Android. I utworzy nam kontroler.
    CONTROLLER - jest tworzony przez VIEW. Jeśli VIEW chce wyświetlić jakąś informację, odwołuje się do kontrolera i to kontroler się tym zajmuje.
    Jeśli VIEW chce wyświetlić listę państw, zwraca się do kontrolera o getCountries(). Kontroler zwraca się do modelu.
    
    MODEL - mamy zaimplementowany. Widać że on zajmuje się zapytaniem i pobraniem listy państw.
    
    Model odwołuje się do API. MVC jest zatem jako "local", a API jako "remote" (tak na rysunku).
    API zwraca Modelowi listę Country.
    Model zwraca listę kontrolerowi.
    Kontroler zwraca listę do View i mówi masz, wyświetl to.
    
    Mamy tu widoczną separację pomiędzy widokiem a kontrolerem.
    Jest tak dlatego żebyśmy w tym przypadku mogli testować View osobno.
    Problem jest taki:
    Widać, że mamy bezpośrednie połączenie pomiędzy View a Controller.
    View wie dokładnie z którym obiektem kontrolera jest połączony, a kontroler to samo. Wie, z którym widokiem jest konkretnie połączony.
    Mamy tutaj zatem coś jak relacja jeden do jednego pomiędzy View a Controller.
    Więc jeśli chcemy zmienić View później w projekcie, musimy to zmienić w tym konkretnym obiekcie View. Nie możemy po prostu oddać tego do innej klasy.
    To jest jedna z wad tego rozwiązania.
    Tak czy inaczej, zróbmy to.

--- app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.java
+++ app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.java
@@ -8,6 +8,29 @@ import android.os.Bundle;
 
 import com.example.androidarchitectures.R;
 
+// MVC - MODEL VIEW CONTROLLER
+
+// VIEW - to widok czyli to co daje nam tutaj MVCActivity. To się robi jako pierwsze przez nasz sytem Android. I utworzy nam kontroler.
+// CONTROLLER - jest tworzony przez VIEW. Jeśli VIEW chce wyświetlić jakąś informację, odwołuje się do kontrolera i to kontroler się tym zajmuje.
+// Jeśli VIEW chce wyświetlić listę państw, zwraca się do kontrolera o getCountries(). Kontroler zwraca się do modelu.
+
+// MODEL - mamy zaimplementowany. Widać że on zajmuje się zapytaniem i pobraniem listy państw.
+
+// Model odwołuje się do API. MVC jest zatem jako "local", a API jako "remote" (tak na rysunku).
+// API zwraca Modelowi listę Country.
+// Model zwraca listę kontrolerowi.
+// Kontroler zwraca listę do View i mówi masz, wyświetl to.
+
+// Mamy tu widoczną separację pomiędzy widokiem a kontrolerem.
+// Jest tak dlatego żebyśmy w tym przypadku mogli testować View osobno.
+// Problem jest taki:
+// Widać, że mamy bezpośrednie połączenie pomiędzy View a Controller.
+// View wie dokładnie z którym obiektem kontrolera jest połączony, a kontroler to samo. Wie, z którym widokiem jest konkretnie połączony.
+// Mamy tutaj zatem coś jak relacja jeden do jednego pomiędzy View a Controller.
+// Więc jeśli chcemy zmienić View później w projekcie, musimy to zmienić w tym konkretnym obiekcie View. Nie możemy po prostu oddać tego do innej klasy.
+// To jest jedna z wad tego rozwiązania.
+// Tak czy inaczej, zróbmy to.
+
 public class MVCActivity extends AppCompatActivity {
 
     @Override
